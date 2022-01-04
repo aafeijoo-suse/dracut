c76c791f6560748f4d9330c7483a24e5be6bb88c
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2022-01-04 16:19:49 +0100

    test(TEST-15-BTRFSRAID): openQA patch

--- test/TEST-15-BTRFSRAID/test.sh
+++ test/TEST-15-BTRFSRAID/test.sh
@@ -4,37 +4,15 @@ TEST_DESCRIPTION="root filesystem on multiple device btrfs"
 
 KVERSION=${KVERSION-$(uname -r)}
 
-# Uncomment this to debug failures
-#DEBUGFAIL="rd.shell"
-test_run() {
-    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-    declare -a disk_args=()
-    # shellcheck disable=SC2034
-    declare -i disk_index=0
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-1.img raid1
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-2.img raid2
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-3.img raid3
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-4.img raid4
-
-    "$testdir"/run-qemu \
-        "${disk_args[@]}" \
-        -append "panic=1 oops=panic softlockup_panic=1 systemd.crash_reboot root=LABEL=root rw rd.retry=3 rd.info console=ttyS0,115200n81 selinux=0 rd.shell=0 $DEBUGFAIL" \
-        -initrd "$TESTDIR"/initramfs.testing
-    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success "$TESTDIR"/marker.img || return 1
-}
+export basedir=/usr/lib/dracut
 
 test_setup() {
-    # Create the blank file to use as a root filesystem
-    DISKIMAGE=$TESTDIR/TEST-15-BTRFSRAID-root.img
-    rm -f -- "$DISKIMAGE"
-    dd if=/dev/zero of="$DISKIMAGE" bs=1M count=1024
-
     kernel=$KVERSION
     # Create what will eventually be our root filesystem onto an overlay
     (
         # shellcheck disable=SC2030
         export initdir=$TESTDIR/overlay/source
+        mkdir -p "$initdir"
         # shellcheck disable=SC1090
         . "$basedir"/dracut-init.sh
         (
@@ -78,40 +56,6 @@ test_setup() {
         inst_hook initqueue/finished 01 ./finished-false.sh
     )
 
-    # create an initramfs that will create the target root filesystem.
-    # We do it this way so that we do not risk trashing the host mdraid
-    # devices, volume groups, encrypted partitions, etc.
-    "$basedir"/dracut.sh -l -i "$TESTDIR"/overlay / \
-        -m "bash btrfs udev-rules base rootfs-block fs-lib kernel-modules qemu" \
-        -d "piix ide-gd_mod ata_piix btrfs sd_mod" \
-        --nomdadmconf \
-        --no-hostonly-cmdline -N \
-        -f "$TESTDIR"/initramfs.makeroot "$KVERSION" || return 1
-
-    rm -rf -- "$TESTDIR"/overlay
-
-    # Create the blank files to use as a root filesystem
-    dd if=/dev/zero of="$TESTDIR"/raid-1.img bs=1MiB count=150
-    dd if=/dev/zero of="$TESTDIR"/raid-2.img bs=1MiB count=150
-    dd if=/dev/zero of="$TESTDIR"/raid-3.img bs=1MiB count=150
-    dd if=/dev/zero of="$TESTDIR"/raid-4.img bs=1MiB count=150
-    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-    declare -a disk_args=()
-    # shellcheck disable=SC2034
-    declare -i disk_index=0
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-1.img raid1
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-2.img raid2
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-3.img raid3
-    qemu_add_drive_args disk_index disk_args "$TESTDIR"/raid-4.img raid4
-
-    "$testdir"/run-qemu \
-        "${disk_args[@]}" \
-        -append "root=/dev/fakeroot rw quiet console=ttyS0,115200n81 selinux=0" \
-        -initrd "$TESTDIR"/initramfs.makeroot || return 1
-
-    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-created "$TESTDIR"/marker.img || return 1
-
     (
         # shellcheck disable=SC2031
         export initdir=$TESTDIR/overlay
@@ -121,17 +65,38 @@ test_setup() {
         inst_hook shutdown-emergency 000 ./hard-off.sh
         inst_hook emergency 000 ./hard-off.sh
     )
-    "$basedir"/dracut.sh -l -i "$TESTDIR"/overlay / \
+    # shellcheck disable=SC2211
+    /usr/src/packages/BUILD/dracut-*/dracut.sh -l -i "$TESTDIR"/overlay / \
         -o "plymouth network kernel-network-modules" \
         -a "debug" \
         -d "piix ide-gd_mod ata_piix btrfs sd_mod" \
         --no-hostonly-cmdline -N \
-        -f "$TESTDIR"/initramfs.testing "$KVERSION" || return 1
+        -f /boot/initramfs.testing "$KVERSION" || return 1
+
+    # delete old config
+    sed -i '6,$d' /etc/grub.d/40_custom
+    # copy boot menu entry
+    sed -n '/### BEGIN \/etc\/grub.d\/10_linux ###/,/submenu/p' /boot/grub2/grub.cfg >> /etc/grub.d/40_custom
+    sed -i '/### BEGIN \/etc\/grub.d\/10_linux ###/d' /etc/grub.d/40_custom
+    sed -i '/submenu/d' /etc/grub.d/40_custom
+    # modify it for testing
+    sed -i "s#menuentry .*#menuentry \'dracut testing\' {#" /etc/grub.d/40_custom
+    sed -i 's#initrd *.*#initrd /boot/initramfs.testing#' /etc/grub.d/40_custom
+    sed -i "/linux/s/\${extra_cmdline.*/panic=1 systemd.log_level=debug systemd.log_target=console rd.retry=3 rd.debug console=tty0 rd.shell=0 $DEBUGFAIL/" /etc/grub.d/40_custom
+    sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEMOUT=5/' /etc/default/grub
+    # create new grub config
+    grub2-mkconfig -o /boot/grub2/grub.cfg || return 1
+    grub2-reboot "dracut testing"
+    sleep 10
+    echo -e "\n\n*************************"
+    echo "dracut-root-block-created"
+    echo -e "*************************\n"
 }
 
 test_cleanup() {
+    rm -r "$TESTDIR"
     return 0
 }
 
 # shellcheck disable=SC1090
-. "$testdir"/test-functions
+. /usr/src/packages/BUILD/dracut-*/test/test-functions
