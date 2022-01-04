a2e4f38230da574450a99caa8c171754668bb9bf
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2022-01-04 15:10:16 +0100

    test(TEST-01-BASIC): openQA patch

--- test/TEST-01-BASIC/test-init.sh
+++ test/TEST-01-BASIC/test-init.sh
@@ -4,11 +4,13 @@
 . /lib/dracut-lib.sh
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
+mount -t proc proc /proc
 command -v plymouth > /dev/null 2>&1 && plymouth --quit
 exec > /dev/console 2>&1
 
-echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/disk/by-id/ata-disk_marker
-
+echo
+echo "*************************"
+echo "dracut-root-block-success"
 export TERM=linux
 export PS1='initramfs-test:\w\$ '
 [ -f /etc/mtab ] || ln -sfn /proc/mounts /etc/mtab
@@ -19,6 +21,8 @@ if getargbool 0 rd.shell; then
     strstr "$(setsid --help)" "control" && CTTY="-c"
     setsid $CTTY sh -i
 fi
-echo "Powering down."
+echo "Rebooting to next test"
+echo "*************************"
+sleep 10
 mount -n -o remount,ro /
-poweroff -f
+echo b > /proc/sysrq-trigger
