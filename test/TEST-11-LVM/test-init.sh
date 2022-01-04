72dcd73b78223ba7367a1cc1eac1cda65510504e
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2022-01-04 16:13:24 +0100

    test(TEST-11-LVM): openQA patch

--- test/TEST-11-LVM/test-init.sh
+++ test/TEST-11-LVM/test-init.sh
@@ -7,8 +7,9 @@ export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 command -v plymouth > /dev/null 2>&1 && plymouth --quit
 exec > /dev/console 2>&1
 
-echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/disk/by-id/ata-disk_marker
-
+echo
+echo "*************************"
+echo "dracut-root-block-success"
 export TERM=linux
 export PS1='initramfs-test:\w\$ '
 [ -f /etc/mtab ] || ln -sfn /proc/mounts /etc/mtab
@@ -19,6 +20,8 @@ if getargbool 0 rd.shell; then
     strstr "$(setsid --help)" "control" && CTTY="-c"
     setsid $CTTY sh -i
 fi
-echo "Powering down."
+echo "Rebooting to next test"
+echo "*************************"
+sleep 10
 mount -n -o remount,ro /
-poweroff -f
+echo b > /proc/sysrq-trigger
