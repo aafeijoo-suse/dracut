e97218fc3e4e5c3d5defe4baf2dd2c5552da7e93
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-11-04 13:31:13 +0100

    fix(dracut): rework timeout for devices added via --mount and --add-device
    Currently, dracut adds a systemd dropin that sets an infinite timeout _only_ for
    the underlying persistent devices of each host device that must be active in the
    initrd. E.g.:
    
    ```
    localhost:~ # dracut -f --stdlog 3 --install "/mnt" --mount "/dev/sda1 /mnt btrfs rw,relatime"
    localhost:~ # lsinitrd | grep timeout.conf
    -rw-r--r--   2 root     root            0 Oct 28 14:21 etc/systemd/system/dev-disk-by\x2duuid-0d1b24c2\x2df112\x2d48ef\x2d8442\x2d1001cffc92f0.device.d/timeout.conf
    localhost:~ # ls -l /dev/disk/by-uuid/0d1b24c2-f112-48ef-8442-1001cffc92f0
    lrwxrwxrwx 1 root root 10 Oct 28 11:43 /dev/disk/by-uuid/0d1b24c2-f112-48ef-8442-1001cffc92f0 -> ../../sda1
    ```
    
    It can be verified in the emergency shell:
    
    ```
    sh-5.2# systemctl show --property JobRunningTimeoutUSec /dev/disk/by-uuid/0d1b24c2-f112-48ef-8442-1001cffc92f0
    JobRunningTimeoutUSec=infinity
    sh-5.2# systemctl show --property JobRunningTimeoutUSec /dev/sda1
    JobRunningTimeoutUSec=1min 30s
    ```
    
    This can cause a problem if a user application expects to mount /dev/sda1 in the
    initrd (that's why it was explicitly added `--mount "/dev/sda1 ..."`), but for
    some reason the link is not created within 1min 30s.
    
    (cherry picked from commit https://github.com/dracut-ng/dracut-ng/commit/c79fc8fd0d4b9aaa8e753296cbafdb1f82781d08)
    
    bsc#1231792

--- modules.d/99base/module-setup.sh
+++ modules.d/99base/module-setup.sh
@@ -110,7 +110,7 @@ install() {
 
     ## save host_devs which we need bring up
     if [[ $hostonly_cmdline == "yes" ]]; then
-        if [[ -n ${host_devs[*]} ]]; then
+        if [[ -n ${host_devs[*]} ]] || [[ -n ${user_devs[*]} ]]; then
             dracut_need_initqueue
         fi
         if [[ -f $initdir/lib/dracut/need-initqueue ]] || ! dracut_module_included "systemd"; then
@@ -140,6 +140,22 @@ install() {
 
                     case "$_pdev" in
                         /dev/?*) wait_for_dev "$_pdev" "infinity" ;;
+                        *) ;;
+                    esac
+                done
+
+                for _dev in "${user_devs[@]}"; do
+
+                    case "$_dev" in
+                        /dev/?*) wait_for_dev "$_dev" 0 ;;
+                        *) ;;
+                    esac
+
+                    _pdev=$(get_persistent_dev "$_dev")
+                    [[ $_dev == "$_pdev" ]] && continue
+
+                    case "$_pdev" in
+                        /dev/?*) wait_for_dev "$_pdev" 0 ;;
                         *) ;;
                     esac
                 done
